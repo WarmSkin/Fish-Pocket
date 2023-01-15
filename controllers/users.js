88f24ca9b6d91d3e4beccf5ff99c02ab67135970
Pin Chen <pamyc1876@gmail.com>
2023-01-15 14:14:29 -0500

    Implement Habit's edit,delete,and add functionalities.Put them into related views.

--- controllers/users.js
+++ controllers/users.js
@@ -1,9 +1,89 @@
 import { User } from "../models/user.js";
+import { Habit } from "../models/habit.js";
+import { Fish } from "../models/fish.js";
+import { Specis } from "../models/specis.js";
 
 function index(req, res) {
-  res.render('users/index', { title: 'Users Page' })
+  res.render("users/index", { title: "Users Page" })
+}
+
+function maintenance(req, res) {
+  Specis.find({})
+  .then(specis => {
+    Habit.find({})
+    .then(habits => {
+      res.render("users/maintenance", { 
+        title: "Maintenance",
+        specis,
+        habits,
+      })
+    })
+    .catch(error => {
+      console.log(error)
+      res.redirect('/')
+    })
+  })
+  .catch(error => {
+    console.log(error)
+    res.redirect('/')
+  })
+}
+
+function createHabit(req, res) {
+  Habit.create(req.body)
+  .then(habit => {
+    res.redirect('/users/maintenance')
+  })
+  .catch(error => {
+    console.log(error)
+    res.redirect('/users/maintenance')
+  })
+}
+
+function deleteHabit(req, res) {
+  Habit.findByIdAndDelete(req.params.id)
+  .then(habit => {
+    Specis.find({})
+    .then(specis => {
+      specis.forEach(fishData => {
+        if(fishData.habits)
+          fishData.habits.remove({_id: req.params.id})
+      })
+    })
+    res.redirect('/users/maintenance')
+  })
+  .catch(error => {
+    console.log(error)
+    res.redirect('/users/maintenance')
+  })
+}
+
+function editHabit(req, res) {
+  Habit.findById(req.params.id)
+  .then(habit => {
+    res.render('habit/update', {
+      title: "Habit Detail",
+      habit,
+    })
+  })
+}
+
+function updateHabit(req, res) {
+  Habit.findByIdAndUpdate(req.params.id, req.body)
+  .then(habit => {
+    res.redirect(`/users/${habit._id}/editH`)
+  })
+  .catch(error => {
+    console.log(error)
+    res.redirect(`/users/${habit._id}/editH`)
+  })
 }
 
 export {
     index,
+    maintenance,
+    createHabit,
+    deleteHabit,
+    editHabit,
+    updateHabit,
 }
--- routes/users.js
+++ routes/users.js
@@ -4,6 +4,11 @@ import * as usersCtrl from '../controllers/users.js'
 const router = Router()
 
 router.get('/', usersCtrl.index)
+router.get('/maintenance', usersCtrl.maintenance)
+router.get('/:id/editH', usersCtrl.editHabit)
+router.post('/habits', usersCtrl.createHabit)
+router.delete('/:id/h', usersCtrl.deleteHabit)
+router.put('/:id/updateH', usersCtrl.updateHabit)
 
 export {
   router
