9377e76241590cc92d370c58bcc209c73eb258a6
Pin Chen <pamyc1876@gmail.com>
2023-01-14 22:44:27 -0500

    Implement Specis' edit functionality and the view.

--- controllers/specis.js
+++ controllers/specis.js
@@ -12,6 +12,10 @@ function index(req, res) {
 }
 
 function newSpecis(req, res) {
+  for (const key in req.body) {
+    if(req.body[key] === "") delete req.body[key]
+  }
+
   Specis.create(req.body)
   .then(specis => {
     res.redirect('/specis')
@@ -33,8 +37,35 @@ function deleteSpecis(req, res) {
   })
 }
 
+function edit(req, res) {
+  Specis.findById(req.params.id)
+  .then(fishData => {
+    res.render('specis/edit', {
+      title: "Fish Data",
+      fishData,
+    })
+  })
+}
+
+function update(req, res) {
+  for (const key in req.body) {
+    if(req.body[key] === "") delete req.body[key]
+  }
+
+  Specis.findByIdAndUpdate(req.params.id, req.body)
+  .then(fishData => {
+    res.redirect(`/specis/${fishData._id}/edit`)
+  })
+  .catch(error => {
+    console.log(error)
+    res.redirect(`/specis/${fishData._id}/edit`)
+  })
+}
+
 export {
     index,
     newSpecis as new,
     deleteSpecis as delete,
+    edit,
+    update,
 }
--- routes/specis.js
+++ routes/specis.js
@@ -4,8 +4,10 @@ import * as specisCtrl from '../controllers/specis.js'
 const router = Router()
 
 router.get('/', specisCtrl.index)
+router.get('/:id/edit', specisCtrl.edit)
 router.post('/new', specisCtrl.new)
 router.delete('/:id', specisCtrl.delete)
+router.put('/:id/', specisCtrl.update)
 
 export {
   router
