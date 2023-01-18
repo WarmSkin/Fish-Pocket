fdb20736f02f19d8f6e0b6f437330f4551861b2c
Pin Chen <pamyc1876@gmail.com>
2023-01-18 14:24:43 -0500

    Update the deleteFish function, now it deletes its id from references array.

--- controllers/fish.js
+++ controllers/fish.js
@@ -22,19 +22,12 @@ function index(req, res) {
 function deleteFish(req, res) {
   Fish.findByIdAndDelete(req.params.id)
   .then(fish => {
-    console.log("delete fish here~~~~!!!!", fish)
-    Profile.find(fish.owner._id)
+    Profile.findOneAndUpdate(
+      {_id: fish.owner._id},
+      {$pull: { fishing: fish._id}}
+      )
     .then(profile => {
-      console.log("Owner here!!!!!", profile)
-      profile.fishing.remove({_id: fish._id})
-      profile.save()
-      .then(profile => {
         res.redirect(`/users/${fish.owner._id}/edit`)
-      })
-      .catch(error => {
-        console.log(error)
-        res.redirect(`/users/${fish.owner._id}/edit`)
-      })
     })
     .catch(error => {
       console.log(error)
