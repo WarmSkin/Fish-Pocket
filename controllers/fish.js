4bf34b868e2ba873b3f88fbbdd968af249964287
Pin Chen <pamyc1876@gmail.com>
2023-01-17 22:55:40 -0500

    Implement delete-fish function

--- controllers/fish.js
+++ controllers/fish.js
@@ -1,4 +1,5 @@
 import { Fish } from "../models/fish.js"
+import { Profile } from "../models/profile.js"
 
 function index(req, res) {
   Fish.find({})
@@ -17,7 +18,31 @@ function index(req, res) {
 }
 
 function deleteFish(req, res) {
-  
+  Fish.findByIdAndDelete(req.params.id)
+  .then(fish => {
+    console.log("delete fish here~~~~!!!!", fish)
+    Profile.find(fish.owner._id)
+    .then(profile => {
+      console.log("Owner here!!!!!", profile)
+      profile.fishing.remove({_id: fish._id})
+      profile.save()
+      .then(profile => {
+        res.redirect(`/users/${fish.owner._id}/edit`)
+      })
+      .catch(error => {
+        console.log(error)
+        res.redirect(`/users/${fish.owner._id}/edit`)
+      })
+    })
+    .catch(error => {
+      console.log(error)
+      res.redirect(`/users/${fish.owner._id}/edit`)
+    })
+  })
+  .catch(error => {
+    console.log(error)
+    res.redirect(`/users/${fish.owner._id}/edit`)
+  })
 }
 
 export {
