5890a10586aafc5837f8bd6706eb09c1f4b2b651
Pin Chen <pamyc1876@gmail.com>
2023-01-20 08:55:27 -0500

    Make users need to login to access to any information.

--- routes/species.js
+++ routes/species.js
@@ -1,18 +1,19 @@
 import { Router } from 'express'
 import * as speciesCtrl from '../controllers/species.js'
+import { isLoggedIn } from '../middleware/middleware.js'
 
 const router = Router()
 
-router.get('/', speciesCtrl.index)
-router.get('/:id/edit', speciesCtrl.edit)
+router.get('/', isLoggedIn, speciesCtrl.index)
+router.get('/:id/edit', isLoggedIn, speciesCtrl.edit)
 
-router.post('/new', speciesCtrl.new)
-router.post('/:id/:hid', speciesCtrl.addHabit)
+router.post('/new', isLoggedIn, speciesCtrl.new)
+router.post('/:id/:hid', isLoggedIn, speciesCtrl.addHabit)
 
-router.delete('/:id', speciesCtrl.delete)
-router.delete('/:id/:hid', speciesCtrl.deleteHabit)
+router.delete('/:id', isLoggedIn, speciesCtrl.delete)
+router.delete('/:id/:hid', isLoggedIn, speciesCtrl.deleteHabit)
 
-router.put('/:id/', speciesCtrl.update)
+router.put('/:id/', isLoggedIn, speciesCtrl.update)
 
 export {
   router
